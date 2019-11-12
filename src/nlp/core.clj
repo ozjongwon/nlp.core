1fb531b1ca1b694cfc2e80ddcdc8c8871d4f7aaa
Jongwon Choi <oz.jongwon.choi@gmail.com>
2019-11-12 20:21:37 +1100

    cleanup

--- src/nlp/core.clj
+++ src/nlp/core.clj
@@ -85,35 +85,26 @@
 ;;;
 (ns nlp.core
   (:gen-class)
-  (:require
-   [clojure.string :refer [join]]
-   [clojure.set :refer [intersection]]
-   [nlp.utils :refer [find-in-coll make-keyword atom? str-sexp->sexp]]
-   [nlp.records :refer [make-token-based-operation-result result-class->prototype
+  (:require [camel-snake-kebab.core :refer [->kebab-case-keyword]]
+            [clojure.set :refer [intersection]]
+            [clojure.string :refer [join]]
+            [medley.core :refer [find-first
+                        ;;assoc-some
+                        ]]
+            [nlp.records :refer [make-token-based-operation-result result-class->prototype
                         token-ann->token-map special-token-map
                         operation-keys->result-record prototype->make-token-based-operation-result
                         key->property-dependency annotators-keys->op-dispatch-set]]
-   [camel-snake-kebab.core :refer [->kebab-case-keyword]]
-   [medley.core :refer [find-first
-                        ;;assoc-some
-                        ]])
-  (:import
-   [nlp.records TokenizeResult PosResult LemmaResult NerResult]
-   [java.util Properties]
+            [nlp.utils :refer [find-in-coll make-keyword atom? str-sexp->sexp]])
+  (:import ;; Tokenizers
+   ;;[edu.stanford.nlp.international.arabic.process ArabicTokenizer]
+   ;;[edu.stanford.nlp.trees.international.pennchinese CHTBTokenizer]
+   [edu.stanford.nlp.trees LabeledScoredTreeNode TreeCoreAnnotations$TreeAnnotation]
    [clojure.lang Reflector]
    ;;[clojure.reflect Constructor]
    ;;clojure.lang.Reflector
    [java.io StringReader]
-   ;; Tokenizers
-   ;;[edu.stanford.nlp.international.arabic.process ArabicTokenizer]
-   ;;[edu.stanford.nlp.trees.international.pennchinese CHTBTokenizer]
-   [edu.stanford.nlp.trees LabeledScoredTreeNode TreeCoreAnnotations$TreeAnnotation]
-   [edu.stanford.nlp.semgraph SemanticGraphCoreAnnotations$EnhancedPlusPlusDependenciesAnnotation]
-   ;;[edu.stanford.nlp.international.french.process FrenchTokenizer]
-   [edu.stanford.nlp.process WordTokenFactory CoreLabelTokenFactory
-    DocumentPreprocessor PTBTokenizer TokenizerFactory
-    ;; LexerTokenizer TokenizerAdapter WhitespaceTokenizer WordSegmentingTokenizer
-    ]
+   [edu.stanford.nlp.coref CorefCoreAnnotations$CorefChainAnnotation]
    ;;[edu.stanford.nlp.ie.machinereading.domains.ace.reader RobustTokenizer]
    ;;[edu.stanford.nlp.international.spanish.process SpanishTokenizer]
 
@@ -122,15 +113,19 @@
     CoreAnnotations$PartOfSpeechAnnotation CoreAnnotations$LemmaAnnotation
     CoreAnnotations$MentionsAnnotation CoreAnnotations$NamedEntityTagAnnotation
     CoreLabel TaggedWord Word SentenceUtils]
-
-   [edu.stanford.nlp.sentiment SentimentCoreAnnotations$SentimentAnnotatedTree]
+   ;;[edu.stanford.nlp.international.french.process FrenchTokenizer]
+   [edu.stanford.nlp.process WordTokenFactory CoreLabelTokenFactory
+    DocumentPreprocessor PTBTokenizer TokenizerFactory
+    ;; LexerTokenizer TokenizerAdapter WhitespaceTokenizer WordSegmentingTokenizer
+    ]
    [edu.stanford.nlp.neural.rnn RNNCoreAnnotations]
-
-   [edu.stanford.nlp.coref CorefCoreAnnotations$CorefChainAnnotation]
+   [edu.stanford.nlp.parser.lexparser LexicalizedParser]
    [edu.stanford.nlp.pipeline Annotation StanfordCoreNLP CoreDocument]
-
+   [edu.stanford.nlp.semgraph SemanticGraphCoreAnnotations$EnhancedPlusPlusDependenciesAnnotation]
+   [edu.stanford.nlp.sentiment SentimentCoreAnnotations$SentimentAnnotatedTree]
    [edu.stanford.nlp.tagger.maxent MaxentTagger]
-   [edu.stanford.nlp.parser.lexparser LexicalizedParser])
+   [java.util Properties]
+   [nlp.records TokenizeResult PosResult LemmaResult NerResult])
   (:gen-class :main true))
 
 
@@ -348,30 +343,6 @@
     (->DocumentResult (execute-document-based-operations ann document-prototype document) ;; document
                       (execute-sentence-operations ann sentence token))))
 
-;; :lemma
-(def lemma-paragraph "Similar to stemming is Lemmatization. This is the process of finding its lemma, its form as found in a dictionary.")
-
-(def pos-paragraph "Bill used the force to force the manger to tear the bill in two.")
-(def pos-paragraph2 "AFAIK she H8 cth! BTW had a GR8 tym at the party BBIAM.")
-(def pos-paragraph3 "Whether \"Blue\" was correct or not (it's not) is debatable.")
-
-(def pos-paragraph4 "The voyage of the Abraham Lincoln was for a long time marked by no special incident. But one circumstance happened which showed the wonderful dexterity of Ned Land, and proved what confidence we might place in him. The 30th of June, the frigate spoke some American whalers, from whom we learned that they knew nothing about the narwhal. But one of them, the captain of the Monroe, knowing that Ned Land had shipped on board the Abraham Lincoln, begged for his help in chasing a whale they had in sight.")
-
-;; :ner
-(def ner-paragraph "Joe was the last person to see Fred. He saw him in Boston at McKenzie's pub at 3:00 where he paid $2.45 for an ale. Joe wanted to go to Vermont for the day to visit a cousin who works at IBM, but Sally and he had to look for Fred.")
-
-;;
-;; :sentiment
-;;
-(def review "An overly sentimental film with a somewhat problematic message, but its sweetness and charm are occasionally enough to approximate true depth and grace. ")
-
-(def sam "Sam was an odd sort of fellow. Not prone to angry and not prone to merriment. Overall, an odd fellow.")
-
-(def mary  "Mary thought that custard pie was the best pie in the world. However, she loathed chocolate pie.")
-(def coref1 "He took his cash and she took her change and together they bought their lunch.")
-(def coref2 "Mary felt the earthquake. It shook the entire building. As she sat there, Mary felt the earthquake.")
-(def coref3 "Who is the 32nd president of the United States?")
-
 ;;;
 ;;; Main function
 ;;;
@@ -380,59 +351,3 @@
         pipeline (apply make-pipeline annotators-keys)]
     (.annotate pipeline annotation) ;; side effect
     (execute-annotation-operations annotation (annotators-keys->op-dispatch-set annotators-keys))))
-
-(defonce paragraph "Let's pause, and then reflect.")
-
-(def pipe-ex "The robber took the cash and ran. The policeman chased him down the street. A passerby, watching the action, tripped the thief as he passed by. They all lived happily ever after, except for the thief of course.")
-
-;; (.prettyPrint pipeline annotation *out*)
-
-;; Sentence #1 (4 tokens):
-;; Who are you?
-
-;; Tokens:
-;; [Text=Who CharacterOffsetBegin=0 CharacterOffsetEnd=3 PartOfSpeech=WP]
-;; [Text=are CharacterOffsetBegin=4 CharacterOffsetEnd=7 PartOfSpeech=VBP]
-;; [Text=you CharacterOffsetBegin=8 CharacterOffsetEnd=11 PartOfSpeech=PRP]
-;; [Text=? CharacterOffsetBegin=11 CharacterOffsetEnd=12 PartOfSpeech=.]
-
-;; Constituency parse:
-;; (ROOT
-;;  (SBARQ
-;;   (WHNP (WP Who))
-;;   (SQ (VBP are)
-;;       (NP (PRP you)))
-;;   (. ?)))
-
-
-;; Dependency Parse (enhanced plus plus dependencies):
-;; root(ROOT-0, Who-1)
-;; cop(Who-1, are-2)
-;; nsubj(Who-1, you-3)
-;; punct(Who-1, ?-4)
-;; nil
-
-;;(analyse-text mary :sentiment :parse)
-(def item1 "The basic principles of contract law are discussed in this chapter. These apply to purely commercial transactions (such as between a manufacturing business and its supplier), as well as transactions where one of the parties is a consumer (a consumer is a person who acquires goods or services for personal or household use). Contract law provides a framework to help resolve disputes and enforce rights and obligations. It supports economic growth by giving individuals and businesses the predictability and confidence to trade, associate and invest.")
-
-(def law1 "(1) An employer must not request or require an employee to work more than the following number of hours in a week unless the additional hours are reasonable:
-(a) for a full-time employee—38 hours; or
-(b) for an employee who is not a full-time employee—the lesser of:
-(i) 38 hours; and
-(ii) the employee’s ordinary hours of work in a week.
-Employee may refuse to work unreasonable additional hours
-
-(2) The employee may refuse to work additional hours (beyond those referred to in paragraph (1)(a) or (b)) if they are unreasonable.
-Determining whether additional hours are reasonable")
-
-(def law2 "1. An employer must not request or require an employee to work more than the following number of hours in a week unless the additional hours are reasonable:
-1-1. for a full-time employee—38 hours; or
-1-2. for an employee who is not a full-time employee—the lesser of:
-1-2-1. 38 hours; and
-1-2-2. the employee’s ordinary hours of work in a week.
-Employee may refuse to work unreasonable additional hours
-
-2. The employee may refuse to work additional hours (beyond those referred to in paragraph 1-1 or 1-2) if they are unreasonable.
-Determining whether additional hours are reasonable")
-
-(def law3 "Suppliers must demonstrate , fairness and courtesy , objectivity , and efficiency by Acknowledging a Complaint within 2 Working Days of receipt where the Complaint is made by email .")
